fcfc3ee3a55507cf0fda5fabdd5e7d28faa7c433
Darshan N G <darshanng492@gmail.com>
2026-02-19 15:16:49 +0530

    Create pattrnmatch.java

---
+++ DAY1/pattrnmatch.java
@@ -0,0 +1,12 @@
+public class pattrnmatch {
+
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++){
+            for (int j=5;j>i;j--)
+            System.out.print("*");
+        System.out.println("\n");
+        }
+        
+    }
+}
+
