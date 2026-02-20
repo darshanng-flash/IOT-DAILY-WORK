255bb9e974e64fc4f13cce4a0889119d72850ae7
Darshan N G <darshanng492@gmail.com>
2026-02-20 09:29:47 +0530

    Create prg1.java

---
+++ DAY2/prg1.java
@@ -0,0 +1,24 @@
+package DAY2;
+//armstong//
+
+public class prg1 {
+    public static void main(String[] args) {
+        int num=153;
+        int count=0;
+        double sum=0;
+        int temp=num;
+        while (temp>0){
+            count++;
+            temp=temp/10;
+
+        }
+        temp=num;
+        while (temp>0){
+            int digit=temp%10;
+            sum=sum+Math.pow(digit,count);
+            temp=temp/10;
+        }
+        if(num==sum)
+        System.out.println("Armstrong");
+    }
+}
