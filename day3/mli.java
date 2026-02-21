fe34133bca93a244ebb6fd3cd83ed6edf215610c
Darshan N G <darshanng492@gmail.com>
2026-02-21 11:50:08 +0530

    d3

--- day3/mli.java
+++ day3/mli.java
@@ -1,41 +1,49 @@
-//multilevel inheritence
+// Multilevel Inheritance
 package day3;
-class Parent{
+
+class Parent {
     String name;
     int age;
-    Parent(String name, int age){
+    int roll;
+
+    Parent(String name, int age, int roll) {
         this.name = name;
         this.age = age;
-    }
-    void display(){
-        System.out.print(name+" "+age);
-    }
-}
-class Child extends Parent{ int roll;
-    Child(String name, int age, int roll){
-        super(name, age);
         this.roll = roll;
     }
-    void show(){
-        super.display();
-        System.out.print(name+" "+age+" "+roll);
+
+    void display() {
+        System.out.print(name + " " + age + " " + roll + " ");
     }
-class SubChild extends Child{
+}
+
+class Child extends Parent {
+
+    Child(String name, int age, int roll) {
+        super(name, age, roll);
+    }
+
+    void show() {
+        super.display();
+    }
+}
+
+class SubChild extends Child {
     int contact;
-    SubChild(String name,int age,int roll,int contact){
-        super(name,age,roll);
+
+    SubChild(String name, int age, int roll, int contact) {
+        super(name, age, roll);
         this.contact = contact;
     }
-    void show1(){
-        System.out.print(name+" "+age+" "+roll+" "+contact);
-    
+
+    void show1() {
+        System.out.print(name + " " + age + " " + roll + " " + contact);
     }
 }
 
 public class mli {
     public static void main(String[] args) {
-        SubChild c= new SubChild("Rohit", 20, 101, 1234567890);
+        SubChild c = new SubChild("Rohit", 20, 101, 1234567890);
         c.show1();
     }
-}
 }
