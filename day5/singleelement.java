86b33cb9a084bee9c0e50a7d241cee6e799c6b15
Darshan N G <darshanng492@gmail.com>
2026-02-28 11:51:26 +0530

    Update singleelement.java

--- day5/singleelement.java
+++ day5/singleelement.java
@@ -6,14 +6,35 @@ Your solution must run in O(log n) time and O(1) space.*/
 package day5;
 
 public class singleelement {
-
     public int singleNonDuplicate(int[] nums) {
-        
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if mid is even or odd and compare with the next element
+            if (mid % 2 == 0) {
+                if (nums[mid] == nums[mid + 1]) {
+                    left = mid + 2; // Move right
+                } else {
+                    right = mid; // Move left
+                }
+            } else {
+                if (nums[mid] == nums[mid - 1]) {
+                    left = mid + 1; // Move right
+                } else {
+                    right = mid - 1; // Move left
+                }
+            }
+        }
+
+        return nums[left]; // The single element
     }
 
     public static void main(String[] args) {
         singleelement obj = new singleelement();
-        System.out.println(obj.singleNonDuplicate(new int[]{1,1,2,3,3,4,4,8,8})); // Output: 2
-        System.out.println(obj.singleNonDuplicate(new int[]{3,3,7,7,10,11,11})); // Output: 10
+        System.out.println(obj.singleNonDuplicate(new int[]{1,1,2,3,3,4,4,8,8})); // 2
+        System.out.println(obj.singleNonDuplicate(new int[]{3,3,7,7,10,11,11})); // 10
     }
 }
