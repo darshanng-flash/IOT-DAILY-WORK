8e335f73260454ba70932fccc1c29d6dcea5dc06
Darshan N G <darshanng492@gmail.com>
2026-03-14 16:09:51 +0530

    12

---
+++ day12/nextgreaterelement.java
@@ -0,0 +1,25 @@
+package day12;
+import java.util.Stack;
+
+public class nextgreaterelement {
+    public static void printNextGreater(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " --> " + result[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 2, 10};
+        printNextGreater(arr);
+    }
+} 
